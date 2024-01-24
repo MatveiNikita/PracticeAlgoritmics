1255905121d0d889acc44b0d1d33e09fdb48c4ad
Matvei Nikita <matveinikitaivanovich@gmail.com>
2024-01-24 08:53:30 +0200

    Algorithm of binarySearch

---
+++ src/binarySearch/BinarySearch.java
@@ -0,0 +1,30 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] x = new int[]{12,41452,4633,574,2,34,266,8979,5656,456,3458,989,56,4,432,67,858}; //example
+        Arrays.sort(x); //it's very important
+        System.out.println(binarySearch(x,67));
+
+    }
+    static int binarySearch(int [] sortedArray, int element){
+
+        int left = 0; // set left border
+        int right =sortedArray.length-1; // set right border
+
+        while (left<=right){
+            int middle = (left+right) / 2; // set middle position
+            int current = sortedArray[middle]; // set current position
+            
+            if(current == element){ // check if element is current position
+                return middle; // return middle position
+            } else if (current < element) { // check if element is greater than current
+                left = middle + 1; //move left border
+            }else{
+                right = middle-1; // otherwise move right border
+            }
+        }return -1;
+    } // The Complexity of this algorithm is O(log(N))
+}
